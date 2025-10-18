942101146ecf11169eb264fc17e7c82c15d7fd53
Andy Ctn <cortinandy@gmail.com>
2025-10-18 12:27:37 +0200

    modif import bannerimg

--- src/components/Main/Logements/Carousel/Carousel.jsx
+++ src/components/Main/Logements/Carousel/Carousel.jsx
@@ -38,7 +38,7 @@ function Carousel({logementId}) {
                     />
                 </>
                 )}
-            </div>            
+            </div>
         </>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,18 +2,19 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Main/Banner/Banner'
 import Gallery from '../../components/Main/Home/LogementGallery/LogementGallery'
+import bannerHome from '../../assets/images/Banner_home.png'
 
 function Home() {
-    return (
-        <>
+  return (
+    <>
         <Header />
         <main className="homepage-content">
-            <Banner image="../../../src/assets/images/Banner_home.png" textBanner="Chez vous, partout et ailleurs" />
-            <Gallery />
-        </main>
-        <Footer />
-        </>
-    )
+        <Banner image={bannerHome} textBanner="Chez vous, partout et ailleurs" />
+        <Gallery />
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default Home
